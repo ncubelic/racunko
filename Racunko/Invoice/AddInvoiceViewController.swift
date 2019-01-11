df8c5fdf30a154bcb067c66364f35b8a198a4a22
Nikola <nikola.cubelic@ingemark.com>
2019-01-11 15:20:34 +0100

    WIP

--- Racunko/Invoice/AddInvoiceViewController.swift
+++ Racunko/Invoice/AddInvoiceViewController.swift
@@ -90,10 +90,18 @@ extension AddInvoiceViewController: UITableViewDataSource {
         case .textField:
             let cell = tableView.dequeueReusableCell(withIdentifier: "AddInvoiceTextFieldCell", for: indexPath) as! AddInvoiceTextFieldCell
             cell.setup(with: item)
+            cell.textField.tag = indexPath.row
+            cell.textField.delegate = self
             return cell
         case .invoiceItem(let invoiceItem):
             let cell = tableView.dequeueReusableCell(withIdentifier: "InvoiceItemTableViewCell", for: indexPath) as! InvoiceItemTableViewCell
             cell.setup(with: invoiceItem)
+            cell.priceTextField.delegate = self
+            cell.priceTextField.tag = 100 + indexPath.row
+            cell.amountTextField.delegate = self
+            cell.amountTextField.tag = 200 + indexPath.row
+            cell.totalAmountTextField.delegate = self
+            cell.totalAmountTextField.tag = 300 + indexPath.row
             return cell
         default:
             break
@@ -129,5 +137,47 @@ extension AddInvoiceViewController: InvoiceItemHeaderDelegate {
     
     func invoiceItemHeaderDidAddItem() {
         addInvoiceItem()
+    }
+}
+
+extension AddInvoiceViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let item = items[0][textField.tag]
+        
+        switch item.type {
+        case .textField(let placeholder, _):
+            let newValue = ItemType.textField(placeholder: placeholder, text: textField.text)
+            items[0][textField.tag].type = newValue
+            
+            guard textField.tag < 4, let nextTextField = tableView.cellForRow(at: IndexPath(row: textField.tag + 1, section: 0)) as? AddInvoiceTextFieldCell else {
+                view.endEditing(true)
+                return true
+            }
+            nextTextField.textField.becomeFirstResponder()
+            return true
+        case .invoiceItem(let item):
+            switch textField.tag {
+            case 101:
+                let invoiceItemRowIndex = textField.tag / 100 - 1
+                guard let text = textField.text, let amount = Int(text) else { return true }
+                let newValue = InvoiceItem(description: item.description, amount: amount, price: item.price, totalAmount: item.totalAmount, discountPercentage: nil)
+                items[1][invoiceItemRowIndex].type = ItemType.invoiceItem(item: newValue)
+            case 102:
+                let invoiceItemRowIndex = textField.tag / 100 - 1
+                guard let text = textField.text, let price = Double(text) else { return true }
+                let newValue = InvoiceItem(description: item.description, amount: item.amount, price: price, totalAmount: item.totalAmount, discountPercentage: nil)
+                items[1][invoiceItemRowIndex].type = ItemType.invoiceItem(item: newValue)
+            case 103:
+                break
+            case 200...203:
+                break
+            case 300...303:
+                break
+            }
+            return true
+        default:
+            return true
+        }
     }
 }
